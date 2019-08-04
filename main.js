5025a3a9880c932ca2c14696da952100d9e39032
CodyPellom <paulpellom@gmail.com>
2019-08-04 14:46:53 -0400

    'init'

--- main.js
+++ main.js
@@ -15,3 +15,18 @@ function menuClick() {
     }
 }
 
+
+var y = 0;
+const backgroundArea = document.getElementById("landerImage");
+backgroundArea.style.backgroundImage= "url('./images/Skillet-Chicken-Fajitas-photo-recipe-picture-1-13.jpg')";
+
+function dynamicBackground() {
+y++
+if (y % 2){
+    backgroundArea.style.backgroundImage= "url('./images/spicy-BBQ-chicken-salad-recipe-4.jpg')";
+}
+else {
+    backgroundArea.style.backgroundImage= "url('./images/Skillet-Chicken-Fajitas-photo-recipe-picture-1-13.jpg')";
+}
+}
+setInterval(dynamicBackground, 3000);
