ca7294ec82a9eaf7025a3fd9ac7c6ab9ae7414c6
CodyPellom <paulpellom@gmail.com>
2019-08-04 20:27:43 -0400

    'init'

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var x = 0;
 function menuClick() {
     x++;
     if (x % 2){
-    firstDisplay = HamMenu.style.height = "50px"; 
+    firstDisplay = HamMenu.style.height = "100px"; 
 }
     else {
         firstDisplay = HamMenu.style.height = "0px";
@@ -19,14 +19,27 @@ function menuClick() {
 var y = 0;
 const backgroundArea = document.getElementById("landerImage");
 backgroundArea.style.backgroundImage= "url('./images/Skillet-Chicken-Fajitas-photo-recipe-picture-1-13.jpg')";
-
 function dynamicBackground() {
 y++
-if (y % 2){
-    backgroundArea.style.backgroundImage= "url('./images/spicy-BBQ-chicken-salad-recipe-4.jpg')";
+if ( y >= 6 ){
+    y = 0;
 }
-else {
-    backgroundArea.style.backgroundImage= "url('./images/Skillet-Chicken-Fajitas-photo-recipe-picture-1-13.jpg')";
+
+if ( y == 1 ){
+    backgroundArea.style.backgroundImage= "url('./images/KetoFood-1.png')";
+}
+else if ( y == 2 ) {
+    backgroundArea.style.backgroundImage= "url('./images/KetoFood-2.png')";
+}
+else if ( y == 3 ){
+    backgroundArea.style.backgroundImage= "url('./images/KetoFood-3.png')";
+}
+else if ( y == 4 ){
+   backgroundArea.style.backgroundImage = "url('./images/KetoFood-4.png')"; 
+}
+else if ( y == 5 ){
+    backgroundArea.style.backgroundImage = "url('./images/KetoFood-5.png')"; 
+    
 }
 }
 setInterval(dynamicBackground, 3000);
