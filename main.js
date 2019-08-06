6bd61b3f1e17ed4b4e3ffa8901bc39d6b0cad924
CodyPellom <paulpellom@gmail.com>
2019-08-06 18:36:09 -0400

    'init2'

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
 
-const HamMenu = document.getElementById("HamburgerMenu");
-var menuIcon = document.getElementById("cart-logo");
-
-var firstDisplay = HamMenu.style.height = "0px";
-
-var x = 0;
-function menuClick() {
-    x++;
-    if (x % 2){
-    firstDisplay = HamMenu.style.height = "100px"; 
-}
-    else {
-        firstDisplay = HamMenu.style.height = "0px";
-    }
-}
-
-
 var y = 0;
 const backgroundArea = document.getElementById("landerImage");
 backgroundArea.style.backgroundImage= "url('./images/Skillet-Chicken-Fajitas-photo-recipe-picture-1-13.jpg')";
