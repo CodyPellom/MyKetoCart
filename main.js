1267bb8d3d900bc51b9b6b9faed16e613cc6a3cd
Cody Pellom <cpellom@appliedsystems.com>
2019-09-20 16:06:51 -0400

    Deleted and Fixing some stuff

--- main.js
+++ main.js
@@ -1,56 +1,21 @@
-// var y = 0;
-// const backgroundArea = document.getElementById("landerImage");
-// backgroundArea.style.backgroundImage= "url('./images/Skillet-Chicken-Fajitas-photo-recipe-picture-1-13.jpg')";
-// function dynamicBackground() {
-// y++
-// if ( y >= 6 ){
-//     y = 0;
-// }
-
-// if ( y == 1 ){
-//     backgroundArea.style.backgroundImage= "url('./images/KetoFood-1.png')";
-// }
-// else if ( y == 2 ) {
-//     backgroundArea.style.backgroundImage= "url('./images/KetoFood-2.png')";
-// }
-// else if ( y == 3 ){
-//     backgroundArea.style.backgroundImage= "url('./images/KetoFood-3.png')";
-// }
-// else if ( y == 4 ){
-//    backgroundArea.style.backgroundImage = "url('./images/KetoFood-4.png')"; 
-// }
-// else if ( y == 5 ){
-//     backgroundArea.style.backgroundImage = "url('./images/KetoFood-5.png')"; 
+var blockOne = document.getElementById("body-display-square-one");
+var blockOneHeader = document.getElementById("body-display-square-one-h1");
+var blockOnePTag = document.getElementById("body-display-square-one-p");
+blockOneHeader.style.display = "none";
+blockOnePTag.style.display = "none";
+blockOne.onmouseover = function(){
+    blockOne.style.background = "purple";
+    blockOneHeader.style.display = "block"
+    blockOnePTag.style.display = "block";
+    blockOneHeader.style.color = "white";
+    blockOnePTag.style.color = "white";
     
-// }
-// }
-// setInterval(dynamicBackground, 3000);
-
-
-var landingPageArea = document.getElementById("mainLandingSection");
-landingPageArea.style.backgroundImage = "url('./images/Keto-LP-WideImg-1.png')";
-x = 0;
-function dynamicLandingBackground(){
-    x++;
-    if (x >= 6){
-        x = 0;
-    }
-    else if (x == 1){
-        landingPageArea.style.backgroundImage = "url('./images/Keto-LP-WideImg-1.png')";
-        landingPageArea.classList.add ="landing-page-wide-img";
-    }
-    else if (x == 2){
-        landingPageArea.style.backgroundImage = "url('./images/Keto-LP-WideImg-2.png')";
-        landingPageArea.classList.add ="landing-page-wide-img";
-    }
-    else if (x == 3){
-        landingPageArea.style.backgroundImage = "url('./images/Keto-LP-WideImg-3.png')";
-        landingPageArea.classList.add ="landing-page-wide-img";
-    }
-    else if (x == 4){
-        landingPageArea.style.backgroundImage = "url('./images/Keto-LP-WideImg-4.png')";
-        landingPageArea.classList.add ="landing-page-wide-img";
-    }
-
 }
-setInterval(dynamicLandingBackground, 4000);
+blockOne.onmouseleave = function(){
+    blockOne.style.backgroundImage = "./images/KetoFood-1.png";
+    blockOneHeader.style.display = "none"
+    blockOnePTag.style.display = "none";
+
+    
+    
+}
